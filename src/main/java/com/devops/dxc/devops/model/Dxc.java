098bb06956525aebaf43df9b86fb51031bdf1100
Marcelo Contreras <mcontreras@transbank.cl>
2022-03-26 00:21:59 -0300

    se agregan test unitarios. Tambien se corrigen variados probleamas de desarrollo y alertas de sonar

--- src/main/java/com/devops/dxc/devops/model/Dxc.java
+++ src/main/java/com/devops/dxc/devops/model/Dxc.java
@@ -9,7 +9,6 @@ public class Dxc implements Serializable{
 	 */
 	private static final long serialVersionUID = -2988002029080131424L;
 	
-	@SuppressWarnings("unused")
 	private int dxc;
 	private int saldo;
 	private Float impuesto;
@@ -17,18 +16,21 @@ public class Dxc implements Serializable{
 	private int ahorro;
 
 	public Dxc(int ahorro, int sueldo){
+
+		Util util = new Util();
+
 		this.ahorro = ahorro;
 		this.sueldo = sueldo;
-		this.impuesto=Util.impuesto(sueldo);
-		this.dxc=Util.getDxc(ahorro, sueldo);
-		this.saldo= ahorro - Util.getDxc(ahorro, sueldo);
+		this.impuesto=util.impuesto(sueldo);
+		this.dxc=util.getDxc(ahorro);
+		this.saldo= ahorro - util.getDxc(ahorro);
 	}
 
 	public Dxc() {
 	}
 
 	public int getDxc() {
-		return Util.getDxc(ahorro,sueldo);
+		return new Util().getDxc(ahorro);
 	}
 	public void setDxc(int dxc) {
 		this.dxc = dxc;
--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -1,22 +1,22 @@
 package com.devops.dxc.devops.model;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
 public class Util {
+
+    Logger logger = LoggerFactory.getLogger(Util.class);
+    
+
     /**
      * Método para cacular el 10% del ahorro en la AFP.  Las reglas de negocio se pueden conocer en 
      * https://www.previsionsocial.gob.cl/sps/preguntas-frecuentes-nuevo-retiro-seguro-10/
@@ -32,25 +32,25 @@ public class Util {
      * podrán hacer el retiro total de fondos en una cuota.
      * 
      */
-    public static int getDxc(int ahorro, int sueldo){
+    public int getDxc(int ahorro){
         if(((ahorro*0.1)/getUf()) > 150 ){
-            return (int) (150*getUf()) ;
+            return (150*getUf()) ;
         } else if((ahorro*0.1)<=1000000 && ahorro >=1000000){
-            return (int) 1000000;
+            return 1000000;
         } else if( ahorro <=1000000){
-            return (int) ahorro;
+            return ahorro;
         } else {
             return (int) (ahorro*0.1);
         }
     }
 
-    public static int diezxciento(int ahorro){
+    public int diezxciento(int ahorro){
 		double diezxciento = ahorro * 0.1;
-		System.out.println(diezxciento);
+		logger.info("diezxciento: {}", diezxciento);
 		return (int) diezxciento;
 	}
 	
-	public static Float impuesto(int sueldo){
+	public Float impuesto(int sueldo){
 		Integer sueldoAnual = sueldo*12;
 		
 		/* 	0,08 si estás en el tercer tramo (renta anual entre $17.864.280 y $29.773.800 de pesos anuales)
@@ -60,25 +60,23 @@ public class Util {
 			0,35 si estás en el séptimo (más de 71,4 millones de pesos anuales)*/
 		
 		
-		float inpuesto = 0.0f;
+		float impuesto = 0.0f;
 		if (sueldoAnual > 17864280 && (sueldoAnual < 29773800)){
-			inpuesto =  0.08f;
+			impuesto =  0.08f;
 		}else if(sueldoAnual > 29700000 && (sueldoAnual < 41600000)){
-			inpuesto = 0.135f;
+			impuesto = 0.135f;
 		}else if(sueldoAnual > 41600000 && (sueldoAnual < 53500000)){
-			inpuesto = 0.23f;
+			impuesto = 0.23f;
 		}else if(sueldoAnual > 53500000 && (sueldoAnual < 71400000)){
-			inpuesto = 0.304f;
+			impuesto = 0.304f;
 		}else if(sueldoAnual > 71400000 ){
-			inpuesto = 0.35f;
+			impuesto = 0.35f;
 		}
 		
-		/*System.out.println("Sueldo Anual: "+sueldoAnual); 
-		System.out.println("Inpuesto"+inpuesto); */
-		return inpuesto;
+		return impuesto;
 	}
 	
-	public static int saldoAhorro(int ahorro) {
+	public int saldoAhorro(int ahorro) {
 		double noventaxciento = ahorro * 0.9;
 		return (int) noventaxciento;
 	}
@@ -89,10 +87,7 @@ public class Util {
      * que retorne la UF en tiempo real.  Por ejemplo mindicador.cl
      * @return
      */
-   /* public static int getUf(){
-        return 29000;
-    }*/
-    public static int getUf(){
+    public int getUf(){
         try {
            /* Date date = new Date();
             SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
@@ -112,19 +107,17 @@ public class Util {
             while ((output = br.readLine()) != null) {
                 JSONParser parser = new JSONParser(); 
                 JSONObject coderollsJSONObject = (JSONObject) parser.parse(output.toString());
-                System.out.println(output);
-               // JSONObject coderollsJSONObject = new JSONObject(output);
-               JSONArray jarray = new JSONArray();
-               jarray=(JSONArray) coderollsJSONObject.get("serie");
+                logger.info(output);
+                // JSONObject coderollsJSONObject = new JSONObject(output);
+                JSONArray jarray = (JSONArray) coderollsJSONObject.get("serie");
                 JSONObject serieJSONObject =  (JSONObject) jarray.get(0);
                 Number valorUf = (Number) serieJSONObject.getAsNumber("valor");
-               /// System.out.println("serie: "+ valorUf +"\n");
                 return valorUf.intValue();
             }
             conn.disconnect();
     
         } catch (Exception e) {
-            System.out.println("Exception in NetClientGet:- " + e);
+            logger.error("Exception in NetClientGet:- " + e);
             return 1;
         }
         return 1;
