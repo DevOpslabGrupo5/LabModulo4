ae7b3549ec25710b1c71a2594cb2b93b9853a24a
LuisaHZ <luisa.c.hernandez@gmail.com>
2022-03-27 00:20:51 -0300

    pruebas selenium correccion

--- src/test/java/com/devops/dxc/devops/ApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/ApplicationTests.java
@@ -38,14 +38,16 @@ public class ApplicationTests {
        // driver.navigate().to("https://www.google.com");
     }
 	 
-    
+    //Prueba calculo donde el impuesto es 0
 	@Test
 	public void testCalcularSinImpuesto() {
 		driver.get("http://localhost:9000/register");
 		driver.manage().window().setSize(new Dimension(1366, 728));
 	  driver.findElement(By.id("ahorro")).click();
+	  driver.findElement(By.id("ahorro")).sendKeys("");
 	  driver.findElement(By.id("ahorro")).sendKeys("12000000");
 	  driver.findElement(By.id("sueldo")).click();
+	  driver.findElement(By.id("sueldo")).sendKeys("");
 	  driver.findElement(By.id("sueldo")).sendKeys("1000000");
 	  driver.findElement(By.cssSelector("button")).click();
 	  assertTrue(driver.findElement(By.id("ImptoId")).getText().contains("0.0"));
@@ -53,13 +55,16 @@ public class ApplicationTests {
 	  driver.quit();
 
 	}
+	//Prueba calculo donde el impuesto es mayor a 0
 	@Test
 	public void testCalcularConImpuesto() {
 		driver.get("http://localhost:9000/register");
 		driver.manage().window().setSize(new Dimension(1366, 728));
 	  driver.findElement(By.id("ahorro")).click();
+	  driver.findElement(By.id("ahorro")).sendKeys("");
 	  driver.findElement(By.id("ahorro")).sendKeys("5000000");
 	  driver.findElement(By.id("sueldo")).click();
+	  driver.findElement(By.id("sueldo")).sendKeys("");
 	  driver.findElement(By.id("sueldo")).sendKeys("2800000");
 	  driver.findElement(By.cssSelector("button")).click();
 	  Double impuesto=Double.valueOf(driver.findElement(By.id("ImptoId")).getText());
@@ -68,6 +73,7 @@ public class ApplicationTests {
 	  driver.quit();
 
 	}
+	//Prueba calculo donde el saldo es 0 y por lo tanto el 10% es 0
 	@Test
 	public void testCalcularSinSaldo() {
 		driver.get("http://localhost:9000/register");
@@ -75,6 +81,7 @@ public class ApplicationTests {
 	  driver.findElement(By.id("ahorro")).click();
 	  driver.findElement(By.id("ahorro")).sendKeys("0");
 	  driver.findElement(By.id("sueldo")).click();
+	  driver.findElement(By.id("sueldo")).sendKeys("");
 	  driver.findElement(By.id("sueldo")).sendKeys("800000");
 	  driver.findElement(By.cssSelector("button")).click();
 	  assertTrue(driver.findElement(By.id("MontoId")).getText().contains("0"));
@@ -82,13 +89,16 @@ public class ApplicationTests {
 	  driver.quit();
 
 	}
+	//Prueba calculo cuando quedara con saldo 0 despues de este retiro
 	@Test
 	public void testCalcularSinFondo() {
 		driver.get("http://localhost:9000/register");
 		driver.manage().window().setSize(new Dimension(1366, 728));
 	  driver.findElement(By.id("ahorro")).click();
+	  driver.findElement(By.id("ahorro")).sendKeys("");
 	  driver.findElement(By.id("ahorro")).sendKeys("300000");
 	  driver.findElement(By.id("sueldo")).click();
+	  driver.findElement(By.id("sueldo")).sendKeys("");
 	  driver.findElement(By.id("sueldo")).sendKeys("800000");
 	  driver.findElement(By.cssSelector("button")).click();
 	  assertTrue(driver.findElement(By.id("SaldoId")).getText().contains("0"));
