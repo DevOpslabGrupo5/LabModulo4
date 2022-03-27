b6600376837a518de7fc548fa1c5d4e1c6d3b837
duribef <diego.uribe.f@gmail.com>
2022-03-27 13:27:24 -0300

    uf conectada

--- src/main/java/com/devops/dxc/devops/controller/MainController.java
+++ src/main/java/com/devops/dxc/devops/controller/MainController.java
@@ -15,31 +15,31 @@ import com.devops.dxc.devops.model.Util;
 @EnableAutoConfiguration
 @Controller
 public class MainController {
-     
+
     @GetMapping("/register")
     public String showForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-         
+
         List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
         model.addAttribute("listProfession", listProfession);
-         
+
         return "register_form";
     }
-    
+
     private float impu;
-    
+
     @PostMapping("/register")
     public String submitForm(@ModelAttribute("user") User user) {
-    	System.out.println(user);
-    	
-    	user.setDiezxciento(Util.getDxc(user.ahorro,user.sueldo));
-    	user.setSaldo(user.ahorro-user.diezxciento);
-    	
-    	/*impu=Util.getDxc(user.ahorro,user.sueldo);*/
-    	impu=Util.impuesto(user.sueldo);
-    	user.setImpuesto(impu);
-    	return "Register_success";
+        System.out.println(user);
+
+        user.setDiezxciento(Util.getDxc(user.ahorro, user.sueldo));
+        user.setSaldo(user.ahorro - user.diezxciento);
+
+        /* impu=Util.getDxc(user.ahorro,user.sueldo); */
+        impu = Util.impuesto(user.sueldo);
+        user.setImpuesto(impu);
+        return "register_success";
     }
-     
+
 }
--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -12,13 +12,16 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import org.springframework.web.client.RestTemplate;
+
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
 public class Util {
     /**
-     * Método para cacular el 10% del ahorro en la AFP.  Las reglas de negocio se pueden conocer en 
+     * Método para cacular el 10% del ahorro en la AFP. Las reglas de negocio se
+     * pueden conocer en
      * https://www.previsionsocial.gob.cl/sps/preguntas-frecuentes-nuevo-retiro-seguro-10/
      * 
      * @param ahorro
@@ -26,108 +29,111 @@ public class Util {
      * @return
      * 
      * 
-     * La Ley estableció un mínimo de entre 0 y 35 UF, y un máximo de 150 UF. 
-     * Es decir, entre $1 millón y $4.3 millones aproximadamente. 
-     * Para aquellos que tengan un saldo en su cuenta individual menor a $1 millón, 
-     * podrán hacer el retiro total de fondos en una cuota.
+     *         La Ley estableció un mínimo de entre 0 y 35 UF, y un máximo de 150
+     *         UF.
+     *         Es decir, entre $1 millón y $4.3 millones aproximadamente.
+     *         Para aquellos que tengan un saldo en su cuenta individual menor a $1
+     *         millón,
+     *         podrán hacer el retiro total de fondos en una cuota.
      * 
      */
-    public static int getDxc(int ahorro, int sueldo){
-        if(((ahorro*0.1)/getUf()) > 150 ){
-            return (int) (150*getUf()) ;
-        } else if((ahorro*0.1)<=1000000 && ahorro >=1000000){
+    public static int getDxc(int ahorro, int sueldo) {
+        if (((ahorro * 0.1) / getUf()) > 150) {
+            return (int) (150 * getUf());
+        } else if ((ahorro * 0.1) <= 1000000 && ahorro >= 1000000) {
             return (int) 1000000;
-        } else if( ahorro <=1000000){
+        } else if (ahorro <= 1000000) {
             return (int) ahorro;
         } else {
-            return (int) (ahorro*0.1);
+            return (int) (ahorro * 0.1);
         }
     }
 
-    public static int diezxciento(int ahorro){
-		double diezxciento = ahorro * 0.1;
-		System.out.println(diezxciento);
-		return (int) diezxciento;
-	}
-	
-	public static Float impuesto(int sueldo){
-		Integer sueldoAnual = sueldo*12;
-		
-		/* 	0,08 si estás en el tercer tramo (renta anual entre $17.864.280 y $29.773.800 de pesos anuales)
-			0,135 si estás en el cuarto tramo (renta entre 29,7 a 41,6 millones de pesos anuales)
-			0,23 si estás en el quinto (entre 41,6 y 53,5 millones de pesos anuales)
-			0,304 si estás en el sexto (entre 53,5 y 71,4 millones de pesos anuales)
-			0,35 si estás en el séptimo (más de 71,4 millones de pesos anuales)*/
-		
-		
-		float inpuesto = 0.0f;
-		if (sueldoAnual > 17864280 && (sueldoAnual < 29773800)){
-			inpuesto =  0.08f;
-		}else if(sueldoAnual > 29700000 && (sueldoAnual < 41600000)){
-			inpuesto = 0.135f;
-		}else if(sueldoAnual > 41600000 && (sueldoAnual < 53500000)){
-			inpuesto = 0.23f;
-		}else if(sueldoAnual > 53500000 && (sueldoAnual < 71400000)){
-			inpuesto = 0.304f;
-		}else if(sueldoAnual > 71400000 ){
-			inpuesto = 0.35f;
-		}
-		
-		/*System.out.println("Sueldo Anual: "+sueldoAnual); 
-		System.out.println("Inpuesto"+inpuesto); */
-		return inpuesto;
-	}
-	
-	public static int saldoAhorro(int ahorro) {
-		double noventaxciento = ahorro * 0.9;
-		return (int) noventaxciento;
-	}
-    
-       
+    public static int diezxciento(int ahorro) {
+        double diezxciento = ahorro * 0.1;
+        System.out.println(diezxciento);
+        return (int) diezxciento;
+    }
+
+    public static Float impuesto(int sueldo) {
+        Integer sueldoAnual = sueldo * 12;
+
+        /*
+         * 0,08 si estás en el tercer tramo (renta anual entre $17.864.280 y $29.773.800
+         * de pesos anuales)
+         * 0,135 si estás en el cuarto tramo (renta entre 29,7 a 41,6 millones de pesos
+         * anuales)
+         * 0,23 si estás en el quinto (entre 41,6 y 53,5 millones de pesos anuales)
+         * 0,304 si estás en el sexto (entre 53,5 y 71,4 millones de pesos anuales)
+         * 0,35 si estás en el séptimo (más de 71,4 millones de pesos anuales)
+         */
+
+        float inpuesto = 0.0f;
+        if (sueldoAnual > 17864280 && (sueldoAnual < 29773800)) {
+            inpuesto = 0.08f;
+        } else if (sueldoAnual > 29700000 && (sueldoAnual < 41600000)) {
+            inpuesto = 0.135f;
+        } else if (sueldoAnual > 41600000 && (sueldoAnual < 53500000)) {
+            inpuesto = 0.23f;
+        } else if (sueldoAnual > 53500000 && (sueldoAnual < 71400000)) {
+            inpuesto = 0.304f;
+        } else if (sueldoAnual > 71400000) {
+            inpuesto = 0.35f;
+        }
+
+        /*
+         * System.out.println("Sueldo Anual: "+sueldoAnual);
+         * System.out.println("Inpuesto"+inpuesto);
+         */
+        return inpuesto;
+    }
+
+    public static int saldoAhorro(int ahorro) {
+        double noventaxciento = ahorro * 0.9;
+        return (int) noventaxciento;
+    }
+
     /**
-     * Método que retorna el valor de la UF.  Este método debe ser refactorizado por una integración a un servicio
-     * que retorne la UF en tiempo real.  Por ejemplo mindicador.cl
+     * Método que retorna el valor de la UF. Este método debe ser refactorizado por
+     * una integración a un servicio
+     * que retorne la UF en tiempo real. Por ejemplo mindicador.cl
+     * 
      * @return
      */
-    public static int getUf(){
-        return 29000;
-    }
-  /* public static int getUf(){
+    /*
+     * public static int getUf(){
+     * return 29000;
+     * }
+     */
+    public static int getUf() {
+
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println(formatter.format(date));
+
+        RestTemplate plantilla = new RestTemplate();
+        String jasonData = plantilla.getForObject("https://mindicador.cl/api/uf/" + formatter.format(date),
+                String.class);
+
+        JSONParser jsonParser = new JSONParser();
+
+        Object obj;
+        int valorUF = 0;
+
         try {
-           Date date = new Date();
-            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-            String dateStr = formatter.format(date);
-            System.out.println("Fecha actual: " + dateStr);
-            
-            URL url = new URL("https://mindicador.cl/api/uf/25-03-2022");//your url i.e fetch data from .
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
-            }
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-            BufferedReader br = new BufferedReader(in);
-            String output;
-            while ((output = br.readLine()) != null) {
-                JSONParser parser = new JSONParser(); 
-                JSONObject coderollsJSONObject = (JSONObject) parser.parse(output.toString());
-                System.out.println(output);
-               // JSONObject coderollsJSONObject = new JSONObject(output);
-               JSONArray jarray = new JSONArray();
-               jarray=(JSONArray) coderollsJSONObject.get("serie");
-                JSONObject serieJSONObject =  (JSONObject) jarray.get(0);
-                Number valorUf = (Number) serieJSONObject.getAsNumber("valor");
-               /// System.out.println("serie: "+ valorUf +"\n");
-                return valorUf.intValue();
-            }
-            conn.disconnect();
-    
+            obj = jsonParser.parse(jasonData);
+            JSONObject miIndicadorObject = (JSONObject) obj;
+            JSONArray serieObjectList = (JSONArray) miIndicadorObject.get("serie");
+            JSONObject serieObject = (JSONObject) serieObjectList.get(0); // UF
+
+            valorUF = (int) Math.round((Double) serieObject.get("valor"));
         } catch (Exception e) {
-            System.out.println("Exception in NetClientGet:- " + e);
-            return 1;
+
+            e.printStackTrace();
         }
-        return 1;
-    }*/
-    
+
+        System.out.println(valorUF);
+        return valorUF;
+    }
+
 }
