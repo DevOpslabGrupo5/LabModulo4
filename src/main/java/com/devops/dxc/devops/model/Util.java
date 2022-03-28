211f3f693f07437f11acb6bc28f3afb93eebdc30
Marcelo Contreras <mcontreras@transbank.cl>
2022-03-27 23:31:51 -0300

    se integra postman collection

--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -12,6 +12,13 @@ import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
+
+import javax.net.ssl.SSLContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
 public class Util {
 
     Logger logger = LoggerFactory.getLogger(Util.class);
@@ -99,6 +106,17 @@ public class Util {
         String fechaFormateada = formatter.format(date);
         
         logger.info(fechaFormateada);
+
+        SSLContext context = SSLContext.getInstance("TLSv1.2");
+        context.init(null, null, null);
+        
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setSSLContext(context)
+            .build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        
+        
+        RestTemplate plantilla = new RestTemplate(factory);
+
 
         RestTemplate plantilla = new RestTemplate();
         String jasonData = plantilla.getForObject("https://mindicador.cl/api/uf/" + formatter.format(date),
